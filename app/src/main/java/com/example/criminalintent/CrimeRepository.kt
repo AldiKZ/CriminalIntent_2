c8bbb16e98fb36e09e664bc8788b563db6d70e72
Aldiyar Rakhimzhanov <aldiyar.rakhimzhanov@samgau.com>
2023-06-10 17:54:28 +0600

    commit 10.06.2023 17:55

---
+++ app/src/main/java/com/example/criminalintent/CrimeRepository.kt
@@ -0,0 +1,38 @@
+package com.example.criminalintent
+
+import android.content.Context
+import androidx.room.Room
+import com.example.criminalintent.database.CrimeDataBase
+import java.util.UUID
+
+private const val DATABASE_NAME = "crime-database"
+
+class CrimeRepository private constructor(context: Context) {
+
+    private val dataBase: CrimeDataBase = Room.databaseBuilder(
+        context.applicationContext, // Контекст
+        CrimeDataBase::class.java, // Класс БД
+        DATABASE_NAME // имя файла БД, которую создаст Room
+    ).build()
+
+    private val crimeDao = dataBase.crimeDao()
+
+    fun getCrimes(): List<Crime> = crimeDao.getCrimes()
+
+    fun getCrime(uuid: UUID): Crime? = crimeDao.getCrime(uuid)
+
+    companion object {
+        private var INSTANCE: CrimeRepository? = null
+
+        fun initialize(context: Context) {
+            if (INSTANCE == null) {
+                INSTANCE = CrimeRepository(context)
+            }
+        }
+
+        fun get(): CrimeRepository {
+            return INSTANCE ?: throw IllegalStateException("CrimeRepository must be initialized")
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/criminalintent/database/CrimeTypeConverters.kt
@@ -0,0 +1,30 @@
+package com.example.criminalintent.database
+
+import androidx.room.TypeConverter
+import java.util.Date
+import java.util.UUID
+
+class CrimeTypeConverters {
+
+    @TypeConverter
+    fun toDate(millisInEpoch: Long?): Date? {
+        return millisInEpoch?.let {
+            Date(it)
+        }
+    }
+
+    @TypeConverter
+    fun fromDate(date: Date?): Long? {
+        return date?.time
+    }
+
+    @TypeConverter
+    fun toUUID(uuid: String?): UUID? {
+        return UUID.fromString(uuid)
+    }
+
+    @TypeConverter
+    fun fromUUID(uuid: UUID?): String? {
+        return uuid?.toString()
+    }
+}
